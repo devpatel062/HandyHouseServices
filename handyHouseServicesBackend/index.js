dbf0b751865453a9441c5e87cb7d2cdbefdc19e5
Dev Patel <patedevj@iu.edu>
2025-02-11 13:02:34 -0500

    uncomment

--- handyHouseServicesBackend/index.js
+++ handyHouseServicesBackend/index.js
@@ -12,20 +12,20 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// const userRoutes = require("./routes/loginUserroute");
-// app.use("/api/users", userRoutes);
+const userRoutes = require("./routes/loginUserroute");
+app.use("/api/users", userRoutes);
 
-// const signupuserRoute = require("./routes/signupuserroute");
-// app.use("/api", signupuserRoute);
+const signupuserRoute = require("./routes/signupuserroute");
+app.use("/api", signupuserRoute);
 
-// const loginUserRoute = require("./routes/loginUserroute")
-// app.use("/api",loginUserRoute)
+const loginUserRoute = require("./routes/loginUserroute")
+app.use("/api",loginUserRoute)
 
-// const repairServicesRoute = require("./routes/repairserviceroute")
-// app.use("/api",repairServicesRoute)
+const repairServicesRoute = require("./routes/repairserviceroute")
+app.use("/api",repairServicesRoute)
 
-// const getuserRoute = require("./routes/getuserroute")
-// app.use("/api",getuserRoute)
+const getuserRoute = require("./routes/getuserroute")
+app.use("/api",getuserRoute)
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
