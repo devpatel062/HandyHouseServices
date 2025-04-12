551f565a2437608feaacf68c1ef7114dd34c2285
devpatel062 <devjpatel2002@gmail.com>
2025-04-12 19:46:31 -0400

    updated modal.js

--- handyHouseServicesBackend/models/provider.js
+++ handyHouseServicesBackend/models/provider.js
@@ -11,7 +11,7 @@ const providerSchema = new mongoose.Schema({
         required: true,
         trim: true,
     },
-    contactNumber: {
+    phone: {
         type: String,
         required: true,
         trim: true,
@@ -22,7 +22,7 @@ const providerSchema = new mongoose.Schema({
         unique: true,
         trim: true,
     },
-    address: {
+    location: {
         type: String,
         required: true,
         trim: true,
