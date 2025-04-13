373962ca0bd53a72c1700761f1447e250e2cf749
devpatel062 <devjpatel2002@gmail.com>
2025-04-13 19:39:58 -0400

    ok

--- handyHouseServicesBackend/routes/getServiceProviderroute.js
+++ handyHouseServicesBackend/routes/getServiceProviderroute.js
@@ -5,26 +5,13 @@ const Provider = require('../models/provider'); // Adjust the path as necessary
 const mongoose = require('mongoose');
 
 // GET /api/providers?serviceType=Plumber
-// router.get('/providers', async (req, res) => {
-//   console.log("Providers route hit", req.query.serviceType)
-//   try {
-//     const { serviceType } = req.query;
-//     const providers = await Provider.find();
-//     res.status(200).json(providers);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Server Error', error });
-//   }
-// });
-
-// routes/provider.js
 router.get('/providers', async (req, res) => {
-  console.log("Providers route hit"); // Should appear in console
+  console.log("Providers route hit", req.query.serviceType)
   try {
-    const providers = await Provider.find(); // No filter
-    console.log("Providers found:", providers); // Log result
+    const { serviceType } = req.query;
+    const providers = await Provider.find();
     res.status(200).json(providers);
   } catch (error) {
-    console.error("Error fetching providers:", error);
     res.status(500).json({ message: 'Server Error', error });
   }
 });
--- handyHouseServicesFrontend/src/assets/pages/serviceproviderModal.jsx
+++ handyHouseServicesFrontend/src/assets/pages/serviceproviderModal.jsx
@@ -5,9 +5,7 @@ const ProvidersModal = ({ serviceType, onClose }) => {
 
   useEffect(() => {
     console.log(serviceType)
-    fetch('https://handy-house-services-backend.vercel.app/api/providers?serviceType=${serviceType}', {
-      method: 'GET',
-    })
+    axios.get('https://handy-house-services-backend.vercel.app/api/providers?serviceType=${serviceType}',)
       .then(res => res.json())
       .then(data => setProviders(data));
       console.log(providers)
