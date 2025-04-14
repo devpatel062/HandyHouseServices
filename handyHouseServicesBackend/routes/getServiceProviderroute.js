e09dbc3de43c066a8f4aae66898d172a4051e684
devpatel062 <devjpatel2002@gmail.com>
2025-04-14 12:00:27 -0400

    providerroute changes

--- handyHouseServicesBackend/routes/getServiceProviderroute.js
+++ handyHouseServicesBackend/routes/getServiceProviderroute.js
@@ -9,7 +9,10 @@ router.get('/providers', async (req, res) => {
   console.log("Providers route hit", req.query.serviceType)
   try {
     const { serviceType } = req.query;
-    const providers = await Provider.find();
+    const providers = await Provider.find({
+      serviceType: { $regex: new RegExp(serviceType, 'i') }
+    });
+    
     res.status(200).json(providers);
   } catch (error) {
     res.status(500).json({ message: 'Server Error', error });
