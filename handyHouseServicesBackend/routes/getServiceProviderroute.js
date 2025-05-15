14d21d87de92e260e692289f71ee31257249b895
devpatel062 <devjpatel2002@gmail.com>
2025-05-14 20:11:00 -0400

    k

--- handyHouseServicesBackend/routes/getServiceProviderroute.js
+++ handyHouseServicesBackend/routes/getServiceProviderroute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Provider = require('../models/provider');
 const mongoose = require('mongoose');
 
-// GET /api/providers?serviceType=Plumber
+// GET /api/providers?service=Plumber
 router.post('/providers', async (req, res) => {
   const {service} = req.body;
   console.log("Providers route hit", service);
@@ -15,7 +15,7 @@ router.post('/providers', async (req, res) => {
   try {
     
     const providers = await Provider.find({
-      serviceType: { $regex: new RegExp(serviceType, 'i') }
+      service: { $regex: new RegExp(service, 'i') }
     });
     
     res.status(200).json(providers);
